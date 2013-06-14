b27efcf6573776941a59914392928aa812692a78
Susan Roberts <sroberts42@gatech.edu>
2013-06-13 23:26:56 -0400

    R2/R3 Changes

--- src/main/java/edu/gatech/cs2340team2/risk/model/RiskGame.java
+++ src/main/java/edu/gatech/cs2340team2/risk/model/RiskGame.java
@@ -1,3 +1,5 @@
+package edu.gatech.cs2340team2.risk.model;
+
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Random;
@@ -5,32 +7,35 @@ import java.util.Random;
 
 public class RiskGame {
 	
-	Queue list = new LinkedList();
+	Queue<Player> list = new LinkedList<Player>();
 	int armies;
 	
-	public Queue players(String[] names){
+	public void initPlayers(String[] names){
 		Random rand = new Random();
 		while(list.size() < names.length){
 			int index = rand.nextInt(names.length);
 			if (names[index] != null){
-				list.add(names[index]);
+				list.add(new Player(names[index], getStartingArmies(names.length)));
 				names[index] = null;
 			}
 		}
-		return list;
 	}
-	public int armies(){
-		switch (list.size()){
-		case 3: armies = 35;
-			break;
-		case 4: armies = 30;
-			break;
-		case 5: armies = 25;
-			break;
-		case 6: armies = 20;
-			break;
+	public int getStartingArmies(int i){
+		switch (i){
+		case 3: 
+			return 35;
+		case 4:
+			return 30;
+		case 5: 
+			return 25;
+		case 6: 
+			return 20;
+		default: 
+			return 30;
 		}
-		return armies;
+	}
+	public Queue<Player> getQueue(){
+		return list;
 	}
 		
 }
