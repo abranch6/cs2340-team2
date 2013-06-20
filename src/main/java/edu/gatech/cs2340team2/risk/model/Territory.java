a29224ee29ad955d0a4f93f2b773e8ed93a3bebc
hlouis3 <hlouis3@gatech.edu>
2013-06-19 22:17:50 -0300

    Territory java for M2

--- src/main/java/edu/gatech/cs2340team2/risk/model/Territory.java
+++ src/main/java/edu/gatech/cs2340team2/risk/model/Territory.java
@@ -1,11 +1,50 @@
 package edu.gatech.cs2340team2.risk.model;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+
 public class Territory
 {
-    MapLocation location;
+
+    private MapLocation location;
+    private int playerId;
+    private int armies;
+    Gson json;
     
     public Territory(int r, int c)
     {
         location = new MapLocation(r,c);
+        json = new GSon();
+    }
+
+    public int getPlayerId()
+    {
+	return playerId;
+    }
+
+    public void setPlayerId(int playerId)
+    {
+	this.playerId = playerId;
+    }
+
+    public void addArmies(int a)
+    {
+	armies += a;
+    }
+
+    public int getArmies()
+    {
+	return armies;
+    }
+
+    public void setArmies(int armies)
+    {
+	this.armies = armies;
+    }
+
+    public String getJSonTerritory()
+    {
+	return json.toJson(location);
     }
 }
