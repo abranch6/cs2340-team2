55a156f4aeb2946113a7e0a8aaf2dc72194b626a
chris-millar <chris.millar@gatech.edu>
2013-06-22 14:57:16 -0300

    Fixed bad-merge Player.java

--- src/main/java/edu/gatech/cs2340team2/risk/model/Player.java
+++ src/main/java/edu/gatech/cs2340team2/risk/model/Player.java
@@ -4,23 +4,14 @@ public class Player
 {
 	private String name;
 	private int armies;
-<<<<<<< HEAD
-	
-
-	public Player(String name, int startingArmies)
-	{
-		this.name = name;
-		this.armies = startingArmies;
-=======
 	private int id;
-	
+
 
 	public Player(String name, int startingArmies, int id)
 	{
 		this.name = name;
 		this.armies = startingArmies;
 		this.id = id;
->>>>>>> upstream/master
 	}
 
 	public String getName()
@@ -37,29 +28,22 @@ public class Player
 		this.armies = armies;
 	}
 
-<<<<<<< HEAD
-=======
 	public void addArmies(int armies)
 	{
 	    this.armies += armies;
 	}
->>>>>>> upstream/master
 	public int getArmies()
 	{
 		return armies;
 	}
-<<<<<<< HEAD
-}
-=======
-	
+
 	public int getId()
 	{
 		return id;
 	}
-	
+
 	public void setId(int id)
 	{
 		this.id = id;
 	}
 }
->>>>>>> upstream/master
