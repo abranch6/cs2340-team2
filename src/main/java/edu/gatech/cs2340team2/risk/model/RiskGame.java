7daeb0939a66f884f8887f0b05b54027a31ecb0b
Andrew Branch <abranch6@gatech.edu>
2013-07-22 11:55:48 -0400

    Fixed place armies method

--- src/main/java/edu/gatech/cs2340team2/risk/model/RiskGame.java
+++ src/main/java/edu/gatech/cs2340team2/risk/model/RiskGame.java
@@ -131,21 +131,40 @@ public class RiskGame {
             {
                 if(selectedTerr.getPlayerId() == playerId)
                 {
-                    selectedTerr.addArmies(armies);
+                   selectedTerr.addArmies(armies);
                    players[playerId].addArmies(-armies);
                    success = true;
                 }
-                else if(selectedTerr.getPlayerId() == -1)
+                else if(selectedTerr.getPlayerId() == NEUTRAL)
                 {
-                    selectedTerr.addArmies(armies);
-                    selectedTerr.setPlayerId(playerId);
-                    players[playerId].addArmies(-armies);
-                    players[playerId].incrementNumTerritoriesContolled();
-                    success = true;
+                    boolean valid = false;
+                    int adjacentPlayerId = NEUTRAL;
+                    for (int i = 0; i < 6; i++)
+                    {
+                        MapLocation neighbor = selectedTerr.getMapLocation().getNeighbor(i);
+                        if (neighbor != null)
+                        {
+                            Territory adjacent = map.getTerritory(neighbor);
+                            if (adjacent != null)
+                            {
+                                adjacentPlayerId = adjacent.getPlayerId();
+                                if (adjacentPlayerId == playerId)
+                                    valid = true; 
+                            }
+                        }
+                    }
                     
+                    if (valid)
+                    {
+                        selectedTerr.addArmies(armies);
+                        selectedTerr.setPlayerId(playerId);
+                        players[playerId].addArmies(-armies);
+                        players[playerId].incrementNumTerritoriesContolled();
+                        success = true;                     
+                    }
                 } 
-            }
-	    }
+            }           	    
+        }
 	   
         if(success)
         {
